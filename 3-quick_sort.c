fc1870bcbc36587650b29340d9e229b54a9000a3
MrSavvy1 <destydesty112@gmail.com>
2024-02-23 08:33:02 -0800

    quick sorted

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,11 +8,15 @@
  * Return: void
  */
 
-void lomu_swap(int *m, int *n)
+void lomu_swap(int *m, int *n, int *array, size_t size)
 {
-	int temp = *m;
-	*m = *n;
-	*n = temp;
+	if (*m != *n)
+	{
+		int temp = *m;
+		*m = *n;
+		*n = temp;
+		print_array(array, size);
+	}
 }
 
 /**
@@ -35,10 +39,10 @@ int lomu_partition(int *array, int down, int up)
 		if (array[j] < pivot)
 		{
 			i++;
-			lomu_swap(&array[i], &array[j]);
+			lomu_swap(&array[i], &array[j], array, size);
 		}
 	}
-	lomu_swap(&array[i + 1], &array[up]);
+	lomu_swap(&array[i + 1], &array[up], array, size);
 	return (i + 1);
 }
 
@@ -56,13 +60,13 @@ void quick_sort_recursive(int *array, int down, int up)
 {
 	if (down < up)
 	{
-		int pivot_index = lomu_partition(array, down, up);
-
-		printf("\n");
-		print_array(array, up + 1);
-
-		quick_sort_recursive(array, down, pivot_index - 1);
-		quick_sort_recursive(array, pivot_index + 1, up);
+		int pivot_index = lomu_partition(array, down, up, size);
+/**
+ * 		printf("\n");
+ * 		print_array(array, up + 1);
+ */
+		quick_sort_recursive(array, down, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, up, size);
 	}
 }
 
@@ -78,5 +82,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size <= 1)
 		return;
-	quick_sort_recursive(array, 0, size - 1);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
